ae916ec9ece84750bfcd0de98b04740930d48a12
wgh815600709qq <815600709@qq.com>
2020-06-09 00:01:35 +0800

    添加跨域限制

--- index.js
+++ index.js
@@ -19,6 +19,15 @@ app.use(session({
   secret: 'performance',
   cookie: {maxAge: 60 * 1000 * 30} // Overdue time
 }))
+// 设置跨域
+app.all('*', function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  res.header('Access-Control-Allow-Methods', '*');
+  res.header('Content-Type', 'application/json;charset=utf-8');
+  next();
+})
+
 app.use(Interceptor) // Router Interceptor
 /*
 *  Put routers in the block
